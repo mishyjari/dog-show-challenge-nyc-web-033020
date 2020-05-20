ea22c074e98946c411e7c207feeb23fc04d97d77
github username <github email address>
2020-05-20 10:25:23 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-document.addEventListener('DOMContentLoaded', () => {
+// Fetch all dogs
+const getDogs = () => {
+	return fetch("http://localhost:3000/dogs")
+	.then( res => res.json() )
+	.then( dogs => populateDogsList(dogs) )
+};
 
+// Take an array of dogs and populate the table
+const populateDogsList = dogs => {
+	const dogsTable = document.getElementById('table-body');
+
+	dogs.forEach(dog => {
+		dogsTable.appendChild(createDogRow(dog));
+	})
+};
+
+// Return a populated tr element with data from a single dog 
+const createDogRow = dog => {
+	const dogTableRow = document.createElement('tr');
+	
+	dogTableRow.id = `dog$dog.id`
+	dogTableRow.innerHTML = `
+		<td>${dog.name}</td>
+		<td>${dog.breed}</td>
+		<td>${dog.sex}</td>
+		<td><button>Edit Dog</button></td>
+	`;
+
+	// Add event listener for edit button
+	dogTableRow.querySelector('button').addEventListener('click', () =>{
+	
+	});
+
+	return dogTableRow;
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+	getDogs();
 })
